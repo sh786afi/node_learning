bd866069d77974c810129fb2a805a0f231ffd2c7
Shafi Akhtar <shafi@Shafis-MacBook-Air.local>
2018-12-25 19:41:20 +0530

    user input 1st

--- app.js
+++ app.js
@@ -1,24 +1,46 @@
 console.log('Starting app.js');
 
-const path=require('path');
+//const path=require('path');
 const fs=require('fs');
-const os=require('os');
+//const os=require('os');
 const _=require('lodash');
-const notes=require('./notes'); 
+const notes=require('./notes.js'); 
 
-var pathObj=path.parse(__filename);
-console.log(pathObj);
-// console.log(_.isString(true));
-// console.log(_.isString('Shafi'));
-var filteredArray=_.uniq(['shafi',1,2,3,1,2,3,4,4,5,5,'shafi','akhtar']);
-console.log(filteredArray);
-var totalMem=os.totalmem();
-var freemem=os.freemem();
-var osType=os.type();
-var networkInter=os.networkInterfaces();
+var command =process.argv[2];
+console.log('Command',command);
+console.log(process.argv);
+if(command==='add'){
+    console.log('adding new note');
+}
+else if(command==='list'){
+    console.log('listing all note');
+}
+else if(command==='read'){
+console.log('reading note');
+}
+else if(command==='remove'){
+    console.log('removing note');
+}
+else{
+    console.log('command not recognized');
+}
 
-//console.log('Total memory: '+totalMem);
+// var pathObj=path.parse(__filename);
+// console.log(pathObj);
+// // console.log(_.isString(true));
+// // console.log(_.isString('Shafi'));
+// var filteredArray=_.uniq(['shafi',1,2,3,1,2,3,4,4,5,5,'shafi','akhtar']);
+// console.log(filteredArray);
+// var totalMem=os.totalmem();
+// var freemem=os.freemem();
+// var osType=os.type();
+// var networkInter=os.networkInterfaces();
 
-console.log(`Total Memory: ${totalMem}`);
-console.log(`Free Memory: ${freemem}`);
-console.log(`OS Type: ${osType}`);
+// //console.log('Total memory: '+totalMem);
+
+// console.log(`Total Memory: ${totalMem}`);
+// console.log(`Free Memory: ${freemem}`);
+// console.log(`OS Type: ${osType}`);
+
+
+
